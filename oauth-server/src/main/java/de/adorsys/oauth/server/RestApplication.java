fd43249ae396417f2d318fb1e2631136fa5761ef
Sandro Sonntag <sandro.sonntag@adorsys.de>
2015-11-23 23:55:55 +0100

    update licence header

--- oauth-server/src/main/java/de/adorsys/oauth/server/RestApplication.java
+++ oauth-server/src/main/java/de/adorsys/oauth/server/RestApplication.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (C) 2015 Daniel Straub, Sandro Sonntag, Christian Brandenstein, Francis Pouatcha (sso@adorsys.de, dst@adorsys.de, cbr@adorsys.de, fpo@adorsys.de)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package de.adorsys.oauth.server;
 
 import javax.ws.rs.ApplicationPath;
