6cf6416ae18d3315dbcc836a53dd79f9328d55e8
DGoAdorsys <DGoAdorsys@users.noreply.github.com>
2017-10-12 15:37:24 +0200

    Add a possibility of automatic release deployment with travis (#54)

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,9 +1,13 @@
-if [[ "$TRAVIS_TAG" =~ ^oauth-parent-[[:digit:].]+$ ]] then
-  echo "RELEASE TAG -> publish $TRAVIS_TAG to mvn central"
-  mvn deploy javadoc:javadoc gpg:sign -Prelease -DskipTests -B -U -Pwildfly
+#!/usr/bin/env bash
+set -e
+
+TAG_PATTERN="^oauth-parent-([[:digit:]]+\.)+[[:digit:]]+$"
+
+if [[ ${TRAVIS_TAG} =~ ${TAG_PATTERN} ]]; then
+  echo "RELEASE TAG -> publish $TRAVIS_TAG to mvn central";
+  mvn --settings .travis/settings.xml deploy -Prelease -DskipTests -B -U -Pwildfly;
 else
-  echo "NO RELEASE TAG -> don't publish to mvn central"
-  mvn package -U -Pwildfly
+  echo "NO RELEASE TAG -> don't publish to mvn central";
 fi
 
 
